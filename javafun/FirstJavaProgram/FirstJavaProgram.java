ab176c55134ba8b0aa7116c178bb9e5812ff50ed
Tasneem Arafat <tasneem.arafat@gmail.com>
2021-06-09 10:53:39 +0300

    adding first assignment

---
+++ javafun/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,8 @@
+public class FirstJavaProgram{
+    public static void main(String[] args) {
+        int age = 27;
+        System.out.println("My name is Tasneem arafat");
+        System.out.println("I am "+age+" years old");
+        System.out.println("My hometown is Nablus, Palestine");
+    }   
+}
