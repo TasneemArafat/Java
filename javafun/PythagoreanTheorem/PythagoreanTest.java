9e4a014384d8ec7d08038b538eaf3e3657cfe9b6
Tasneem Arafat <tasneem.arafat@gmail.com>
2021-06-09 11:53:38 +0300

    adding assignment2

---
+++ javafun/PythagoreanTheorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main(String[] args) {
+        PythagoreanTheorem test = new PythagoreanTheorem();
+        double result = test.calculateHypotenuse(3, 4);
+        System.out.println(result);
+    }
+}
